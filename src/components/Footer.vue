759c6d6b9f357dc8d7fa63a5dd374a31480f5a22
Benedict Umeozor <111297090+Bened1ct-7@users.noreply.github.com>
2023-04-22 14:06:20 +0100

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="container">
+      <p>&copy; Benedict 2023</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+    padding: 1.5rem;
+    text-align: center;
+  }
+  
+  p {
+    opacity: 0.75;
+    color: #555;
+    font-size: 0.85rem;
+  }
+</style>
